3faa19ca5740a72f588d2b19ac5ff289c8ff2a8c
Windblade <juliangarzon867@gmail.com>
2025-07-08 19:37:47 -0500

    Pantalla del editor

--- src/main.js
+++ src/main.js
@@ -1,24 +1,92 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as monaco from "monaco-editor";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const explorer = document.getElementById("explorer");
+const menuFlotante = document.getElementById("menuFlotante");
 
-setupCounter(document.querySelector('#counter'))
+const treeDirectory = {
+  name: "root",
+  type: "folder",
+  children: [
+    {
+      name: "emptyfile.js",
+      type: "file",
+      content: "console.log('Hola Mundo')"
+    },
+
+    {
+      name: "other.js",
+      type: "file",
+      content: "let value = 1"
+    },
+
+    {
+      name: "proyecto1",
+      type: "folder",
+      children: [
+        {
+          name: "archivo.py",
+          type: "file",
+          content: "print('Hello')"
+        }
+      ]
+    }
+  ]
+}
+
+const editor = monaco.editor.create(document.getElementById("editor"), {
+  language: "javascript",
+  theme: "vs-dark",
+});
+
+editor.setValue(`console.log("Hello, World!");`);
+
+function renderTree(node, parent) {
+  if (node.type === "file") {
+    const a = document.createElement("li");
+    a.textContent = node.name;
+    a.classList.add(node.type);
+    parent.appendChild(a);
+    a.addEventListener("click", () => {
+      editor.setValue(node.content);
+    });
+  }
+
+  const element = document.createElement("div");
+
+  document.addEventListener("click", () => {
+    menuFlotante.style.display = "none";
+  });
+
+  if (node.type === "folder") {
+    const inner = document.createElement("div");
+    inner.textContent = node.name;
+    inner.classList.add(node.type);
+
+    inner.addEventListener("contextmenu", (e) => contextMenuListener(e));
+    element.appendChild(inner);
+    parent.appendChild(element)
+
+    if (node.children) {
+      const lista = document.createElement("ul");
+      element.appendChild(lista);
+
+      inner.addEventListener("click", () => {
+        lista.classList.toggle("oculto");
+      });
+
+      node.children.forEach((child) => {
+        renderTree(child, lista);
+      });
+    }
+  }
+}
+
+function contextMenuListener(e) {
+  e.preventDefault();
+  menuFlotante.style.display = "flex";
+  menuFlotante.style.left = e.clientX + "px";
+  menuFlotante.style.top = e.clientY + "px";
+
+}
+
+renderTree(treeDirectory, explorer);
